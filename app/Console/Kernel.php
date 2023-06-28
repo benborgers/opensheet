d3f979c326371676044275bf26fdfc89ebc2c860
Ben Borgers <borgersbenjamin@gmail.com>
2023-06-28 09:54:00 -0400

    Add Telescope

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -7,17 +7,11 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
-    /**
-     * Define the application's command schedule.
-     */
     protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('telescope:prune --hours=168')->daily();
     }
 
-    /**
-     * Register the commands for the application.
-     */
     protected function commands(): void
     {
         $this->load(__DIR__.'/Commands');
