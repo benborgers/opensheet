7b55fc2d980fad5ebef640ec916404ffe84de79c
Ben Borgers <borgersbenjamin@gmail.com>
2023-06-28 09:48:36 -0400

    Protect look_up_numeric_sheet

--- routes/api.php
+++ routes/api.php
@@ -15,27 +15,29 @@ if (! function_exists('error')) {
     }
 }
 
-function look_up_numeric_sheet($sheet, $id)
-{
-    if (! is_numeric($sheet)) {
-        return $sheet;
-    }
+if (! function_exists('look_up_numeric_sheet')) {
+    function look_up_numeric_sheet($sheet, $id)
+    {
+        if (! is_numeric($sheet)) {
+            return $sheet;
+        }
 
-    if (intval($sheet) <= 0) {
-        error('For this API, sheet numbers start at 1');
-    }
+        if (intval($sheet) <= 0) {
+            error('For this API, sheet numbers start at 1');
+        }
 
-    $json = Http::get("https://sheets.googleapis.com/v4/spreadsheets/{$id}?key=".config('services.google.key'))->json();
-    if (array_key_exists('error', $json)) {
-        error($json['error']['message']);
-    }
+        $json = Http::get("https://sheets.googleapis.com/v4/spreadsheets/{$id}?key=".config('services.google.key'))->json();
+        if (array_key_exists('error', $json)) {
+            error($json['error']['message']);
+        }
 
-    $sheetWithIndex = $json['sheets'][intval($sheet) - 1] ?? null;
-    if (! $sheetWithIndex) {
-        error("There is no sheet number {$sheet}");
-    }
+        $sheetWithIndex = $json['sheets'][intval($sheet) - 1] ?? null;
+        if (! $sheetWithIndex) {
+            error("There is no sheet number {$sheet}");
+        }
 
-    return $sheetWithIndex['properties']['title'];
+        return $sheetWithIndex['properties']['title'];
+    }
 }
 
 Route::get('{id}/{sheet}', function ($id, $sheet) {
