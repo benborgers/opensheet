63a2442d49ebafb79519702956fda6eb81263416
Ben Borgers <borgersbenjamin@gmail.com>
2023-06-28 09:35:14 -0400

    Get sheet from index

--- config/services.php
+++ config/services.php
@@ -31,4 +31,8 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
+    'google' => [
+        'key' => env('GOOGLE_API_KEY')
+    ],
+
 ];
--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,50 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
 
 Route::redirect('/', 'https://github.com/benborgers/opensheet#readme');
 
+function error($message)
+{
+    abort(response()->json([
+        'error' => $message
+    ], 400));
+}
+
+function look_up_numeric_sheet($sheet, $id)
+{
+    if (!is_numeric($sheet)) {
+        return $sheet;
+    }
+
+    if (intval($sheet) <= 0) {
+        error('For this API, sheet numbers start at 1');
+    }
+
+    $json = Http::get("https://sheets.googleapis.com/v4/spreadsheets/{$id}?key=".config('services.google.key'))->json();
+    if (array_key_exists('error', $json)) {
+        error($json['error']['message']);
+    }
+
+    $sheetWithIndex = $json['sheets'][intval($sheet) - 1] ?? null;
+    if (! $sheetWithIndex) {
+        error("There is no sheet number {$sheet}");
+    }
+
+    return $sheetWithIndex['properties']['title'];
+}
+
 Route::get('{id}/{sheet}', function ($id, $sheet) {
-    return 'Hello world!';
+    $data = Cache::remember("{$id}/{$sheet}", 30, function () use ($id, $sheet) {
+        $sheet = preg_replace('/\+/', ' ', $sheet);
+        $sheet = look_up_numeric_sheet($sheet, $id);
+        return ['sheet' => $sheet];
+    });
+
+    return response()->json($data);
 });
 
 Route::get('{any}', function () {
